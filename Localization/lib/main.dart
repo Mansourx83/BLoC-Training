a608342b51d2e90aeca4d233e35d2543b69f0253
Mansourx83 <yousaf83000@gmail.com>
2024-04-08 13:02:45 +0200

    add new app

---
+++ Localization/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localizations_test_app/cubit/locale_cubit.dart';
+import 'app_localizations.dart';
+import 'home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => LocaleCubit()..getSavedLanguage(),
+        ),
+      ],
+      child: BlocBuilder<LocaleCubit, ChangeLocaleState>(
+        builder: (context, state) {
+          return MaterialApp(
+            //main lang
+            locale: state.locale,
+            //Lang the app support
+            supportedLocales: const [Locale('en'), Locale('ar')],
+            //the direction 
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate
+            ],
+            localeResolutionCallback: (deviceLocale, supportedLocales) {
+              for (var locale in supportedLocales) {
+                if (deviceLocale != null &&
+                    deviceLocale.languageCode == locale.languageCode) {
+                  return deviceLocale;
+                }
+              }
+
+              return supportedLocales.first;
+            },
+            debugShowCheckedModeBanner: false,
+            home: const HomePage(),
+          );
+        },
+      ),
+    );
+  }
+}
